725adadd561cd20d4d0cd2b6e6d40e8518e950f7
Eklemis Santo Ndun <Eklemis.ndun@gmail.com>
2022-03-12 22:20:00 +0800

    try fixing api bug

--- pages/api/user-comments.js
+++ pages/api/user-comments.js
@@ -3,7 +3,7 @@ import { getComments } from "../../helpers/crud";
 export default async function handler(req, res) {
 	if (req.method === "GET") {
 		userName = req.body.userName;
-		const comments = getComments(userName);
+		const comments = await getComments(userName);
 		res.status(200).json({ message: "success", data: comments });
 	}
 	res.status(400).json({ message: "no available response" });
